6f07bfbb04e039aabd4bbcb4a4d508fdce8bdeff
Chris Madison <CKMadison4@gmail.com>
2016-06-21 16:09:04 -0700

    Created object of categories that will be passed to the show.ejs file in categories folder.

---
+++ controllers/categories.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var db = require('../models');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  db.category.findAll().then(function(category){
+  res.send(category);
+
+  })
+});
+
+
+
+module.exports = router;
