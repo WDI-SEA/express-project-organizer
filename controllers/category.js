6d47acf46f51a0bc6dc30e97d1a5a86fcae09826
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-06-21 17:38:06 -0700

    categories page lists categories. category relations to be made

--- controllers/category.js
+++ controllers/category.js
@@ -6,7 +6,24 @@ var ejsLayouts = require('express-ejs-layouts');
 var app = express();
 
 router.get('/', function(req, res) {
-  res.render('show.ejs');
+  db.category.findAll()
+  .then(function(categories) {
+  res.render('categories/show.ejs', { categories: categories })
+  })
 })
+
+// GET /projects/:id - display a specific project
+  router.get('/:id', function(req, res) {
+    db.category.find({
+      where: { id: req.params.id }
+    })
+    .then(function(category) {
+      if (!category) throw Error();
+      res.render('category', { category: category });
+    })
+    .catch(function(error) {
+      res.status(400).render('main/404');
+    });
+  });
 
 module.exports = router;
