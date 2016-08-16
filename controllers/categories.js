a531663b1b16e6fea304732ed26dae07a42ca105
zmcdaniel <zmcdaniel@gmail.com>
2016-08-16 16:38:40 -0700

    made a categories controller and a view folder for categores; currently have categories displaying and linking together

---
+++ controllers/categories.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var db = require('../models');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+ db.category.findAll().then(function(category){
+ res.render('categories/show.ejs', { category: category });
+ })
+});
+
+router.get('/:name', function(req, res) {
+ db.category.find({
+   where: { name: req.params.name },
+   include: [db.project]
+ }).then(function(name) {
+   res.render('categories/details', { name });
+ })
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var db = require('./models');
+
 var app = express();
 
 // Settings
@@ -26,7 +27,7 @@ app.get('/', function(req, res) {
 
 // POST /projects - creates a new project, then redirects back to GET /
 app.post('/projects', function(req, res) {
-  res.send('Route for POST /projects');
+  console.log(req.body);
 });
 
 // GET /projects/new - page that has a form for creating a new project
@@ -41,6 +42,8 @@ app.get('/projects/:id', function(req, res) {
 
 // Controllers
 app.use('/projects', require('./controllers/projects'));
+app.use('/categories', require('./controllers/categories'));
+      
 
 
 // Listening to Port 3000
