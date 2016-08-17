088acefc868cbea274dd4e217efa3e8617538cd6
zmcdaniel <zmcdaniel@gmail.com>
2016-08-16 17:19:53 -0700

    commented out dbTests; linked to categories in navbar

--- dbTest.js
+++ dbTest.js
@@ -1,18 +1,18 @@
-var db = require('./models');
-console.log('Starting test....');
-db.project.find({
-  where: { id: 1 },
-  include: [db.category]
-}).then(function(project) {
-  // by using eager loading, the project model should have a categories key
-  if(project) {
-    console.log(project.categories);
-  } else {
-    console.log("No project here!");
-  }
+// var db = require('./models');
+// console.log('Starting test....');
+// db.project.find({
+//   where: { id: 1 },
+//   include: [db.category]
+// }).then(function(project) {
+//   // by using eager loading, the project model should have a categories key
+//   if(project) {
+//     console.log(project.categories);
+//   } else {
+//     console.log("No project here!");
+//   }
 
-  // a createCategory function should be available to this model
-  project.createCategory({ name: 'node' }).then(function(category) {
-    console.log(category.get());
-  });
-});
+//   // a createCategory function should be available to this model
+//   project.createCategory({ name: 'node' }).then(function(category) {
+//     console.log(category.get());
+//   });
+// });
