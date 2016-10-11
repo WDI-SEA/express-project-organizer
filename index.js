ebf3955f6eab89549ceb094645f7705ed8d15bb8
jedimasterclos <carlossrinivas@gmail.com>
2016-10-11 16:54:42 -0700

    Carlos: Project organized

--- index.js
+++ index.js
@@ -10,13 +10,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
-  db.project.findAll()
-  .then(function(projects) {
-    res.render('main/index', { projects: projects });
+  db.project.findAll( {
+  	include: [db.category]
+  }).then(function(projects) {
+    	res.render('main/index', { projects: projects });
   })
   .catch(function(error) {
     res.status(400).render('main/404');
   });
+});
+
+app.get('/categories', function(req, res) {
+	db.category.findAll()
+	.then(function(categories) {
+		res.render('main/categories', {categories: categories})
+	})
+});
+
+app.get('/categories/:id', function(req, res) {
+	db.category.find( {
+		where: { id: req.params.id}
+	}).then(function(category) {
+		category.getProjects().then(function(projects) {
+			res.render('main/projcat', { categoryName: category.name, projects: projects});
+		})
+	})
 });
 
 app.use('/projects', require('./controllers/projects'));
