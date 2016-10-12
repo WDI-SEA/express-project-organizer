8968ce8d2dfd8f340d64355a74fe10b7732c73f3
alfordsiler <alfordsiler@gmail.com>
2016-10-11 20:41:53 -0700

    express-project-organizer

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
-  db.project.findAll()
+  db.project.findAll({
+  })
   .then(function(projects) {
     res.render('main/index', { projects: projects });
   })
@@ -18,6 +19,26 @@ app.get('/', function(req, res) {
     res.status(400).render('main/404');
   });
 });
+
+//Get to display all categories
+app.get('/categories', function(req, res){
+  db.category.findAll({
+  })
+  .then(function(categories) {
+    res.render('main/categories', { categories: categories })
+  });
+});
+
+//Display a specific catergory
+app.get('/categories/:id', function(req, res){
+  db.category.find({
+    where: { id: req.params.id },
+  }).then(function(category){
+    category.getProjects().then(function(projects){
+      res.render('main/categoryDetails', { categoryName: category.name, projects: projects });
+    });
+  });//end then
+})
 
 app.use('/projects', require('./controllers/projects'));
 
