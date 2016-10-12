42276dc15b7e92604e079a71233c958db0a370d2
tannerkovach <kovach.tanner@gmail.com>
2016-10-12 09:06:08 -0700

    everything works - added delete button

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,19 +4,41 @@ var router = express.Router();
 
 // POST /projects - create a new project
 router.post('/', function(req, res) {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployedLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then(function(project) {
-    res.redirect('/');
-  })
-  .catch(function(error) {
-    res.status(400).render('main/404');
-  });
+  var categories = [];
+    if(req.body.category) { 
+    categories = req.body.category.split(',');
+    }
+    db.project.create({
+      name: req.body.name,
+      githubLink: req.body.githubLink,
+      deployedLink: req.body.deployedLink,
+      description: req.body.description
+    })
+    .then(function(project) {
+      if(categories.length > 0) {
+        for(var i = 0; i < categories.length; i++){
+
+            db.category.findOrCreate({
+            where: {name: categories[i]}
+            }).spread(function(category, wasCreated){
+                  if(category){
+                    project.addCategory(category); // part creats the relation in the map table
+                  }
+
+            }); // end spread
+
+          } // end of loop
+        }
+         // end of if
+        res.redirect("/");
+    }) // then function
+    .catch(function(error) {
+      res.status(400).render('main/404');
+    });
+
 });
+
+
 
 // GET /projects/new - display form for creating a new project
 router.get('/new', function(req, res) {
@@ -36,5 +58,8 @@ router.get('/:id', function(req, res) {
     res.status(400).render('main/404');
   });
 });
+
+
+
 
 module.exports = router;
