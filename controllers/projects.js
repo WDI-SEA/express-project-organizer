318f17c7e62983b564da669d4535d4bdc86400ae
kimmabrandt <kimmabrandt@gmail.com>
2016-10-12 10:39:54 -0700

    deliverable final

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,19 +4,48 @@ var router = express.Router();
 
 // POST /projects - create a new project
 router.post('/', function(req, res) {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployedLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then(function(project) {
-    res.redirect('/');
-  })
-  .catch(function(error) {
-    res.status(400).render('main/404');
-  });
-});
+    var categories = [];
+    if(req.body.categories){
+      var categories = req.body.categories.split(",");        
+    }
+    db.project.create({
+      id: req.params.id,
+      name: req.body.name,
+      githubLink: req.body.githubLink,
+      deployedLink: req.body.deployedLink,
+      description: req.body.description
+      }).then(function(project){
+          if(categories.length > 0){
+            for(var i = 0; i < categories.length; i++){
+              db.category.findOrCreate({
+                where: {name: categories[i]}
+              }).spread(function(category, wasCreated){ //tells you whether tag was created
+                if(category){ //addTag is a HELPER FUNCITON along with createArticle
+                  project.addCategory(category); // This part creates the relation in the map table
+                }
+              }); //End spread
+            } // End loop
+          } // End if
+
+    console.log(project.get());
+          res.redirect("/");
+      }); //End then() for createArticle
+         
+          }); //End then() for author 
+
+ 
+  // console.log(req.body);
+  // res.send("Testing my post route, yay it worked");
+
+
+  // db.project.createArticle({
+  //   name: req.body.name,
+  //   githubLink: req.body.githubLink,
+  //   deployedLink: req.body.deployedLink,
+  //   description: req.body.description,
+  // })
+
+
 
 // GET /projects/new - display form for creating a new project
 router.get('/new', function(req, res) {
