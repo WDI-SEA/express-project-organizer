bcd47c1ed7d6fa9bdfa2d91a6c3da89191f9b2f7
CannedYeti <cpotebnya@gmail.com>
2017-03-01 08:30:50 -0800

    finished no styling

---
+++ controllers/categories.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var db = require('../models');
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    db.category.findAll().then(function(categories){
+        res.render("categories/categories", {categories: categories});  
+    })
+});
+
+router.get("/:id", function(req, res){
+  db.category.findOne({
+    where: {id: req.params.id},
+    include: [db.project]
+  }).then(function(category){
+    res.render("categories/list", {category: category});
+  });
+});
+
+
+
+module.exports = router;
--- controllers/projects.js
+++ controllers/projects.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var db = require('../models');
 var router = express.Router();
+var async = require("async");
 
 // POST /projects - create a new project
 router.post('/', function(req, res) {
+  var categories = [];
+  if (req.body.categories) {
+    categories = req.body.categories.split(",");
+  }
+  console.log(categories);
   db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
@@ -11,7 +17,23 @@ router.post('/', function(req, res) {
     description: req.body.description
   })
   .then(function(project) {
-    res.redirect('/');
+    if(categories.length > 0) {
+      async.forEachSeries(categories, function(c, callback){
+        db.category.findOrCreate({
+          where: {name: c.trim()}
+        }).spread(function(newCat, wasCreated){
+          if(newCat){
+            project.addCategory(newCat);
+          }
+          callback(null); //Give it permission to go to the next iteration
+        });
+      }, function(){
+    res.redirect("/")
+  });
+  }
+    else {
+      res.send("/");
+    }
   })
   .catch(function(error) {
     res.status(400).render('main/404');
