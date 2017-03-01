3fcc8f306d853ab22a66b479e06e9ed664373ca6
jnhustin <jnhustin@gmail.com>
2017-02-28 19:35:22 -0800

    added category view w/ links to project page

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,3 +2,24 @@ var express = require('express');
 var db = require('../models');
 var router = express.Router();
 
+router.get('/', function(req, res) {
+	db.category.findAll()
+	.then(function(categories) {
+		res.render('categories', {categories: categories})
+	});
+})
+router.get('/:id', function(req, res) {
+	db.category.find({
+		where: {id: req.params.id},
+		include: [db.project]
+	})
+	.then(function(categories) {
+		if (!categories) throw Error();
+		res.render('showCategory', {categories: categories});
+	})
+	.catch(function(error) {
+		res.status(400).render('main/404');
+	});
+});
+
+module.exports = router;
