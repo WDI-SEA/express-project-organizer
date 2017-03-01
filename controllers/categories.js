88f73ea12ff6a0fe6323c70ba797df852ccf592c
JRgerv <jack.gervais@gmail.com>
2017-03-01 08:44:34 -0800

    debugging

---
+++ controllers/categories.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var db = require('../models');
+var router = express.Router();
+
+router.get("/", function(req, res){
+  db.category.findAll().then(function(categories){
+    res.render("categories/categories", {categories: categories});
+  });
+});
+
+router.get("/:id", function(req, res){
+  db.category.findOne({
+    where: {id: req.params.id},
+    include: [db.project]
+  }).then(function(tag){
+    res.render("categories/show", {category: category});
+  });
+});
+
+module.exports = router;
