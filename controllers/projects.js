7cd230df16cb8081fb3e2588fd8c27f1c7f88dac
cgnzlz22 <cgnzlz22@yahoo.com>
2017-03-01 16:10:21 -0800

    DONE, no styling

--- controllers/projects.js
+++ controllers/projects.js
@@ -9,11 +9,47 @@ router.post('/', function(req, res) {
     githubLink: req.body.githubLink,
     deployedLink: req.body.deployedLink,
     description: req.body.description
-  })
-  .then(function(project) {
+  }).then(function(project) {
+    if (req.body.catagory) {
+      var categories = req.body.catagory.split(',');
+
+      for (var i = 0; i < categories.length; i++) {
+        db.category.findOrCreate({
+          where: { name: categories[i] }
+        }).spread(function(category, wasCreated) {
+          if (category) {
+            project.addCategory(category);
+          }
+        });
+      }
+    }
     res.redirect('/');
   })
   .catch(function(error) {
+    res.status(400).render('main/404');
+  });
+});
+
+
+// GET projects/categories - All categories displayed
+router.get('/categories', function(req, res) {
+  db.category.findAll({
+    order: 'name ASC'
+  }).then(function(categories) {
+    res.render('categories/categories', { categories: categories });
+  }).catch(function(error) {
+    res.status(400).render('main/404');
+  });
+});
+
+// GET /categories/:id - Display sßpecific category and all the projects within that category
+router.get('/categories/:id', function(req, res) {
+  db.category.findOne({
+    where: { id: req.params.id },
+    include: [db.project]
+  }).then(function(category){
+    res.render('categories/show', { category: category });
+  }).catch(function(error) {
     res.status(400).render('main/404');
   });
 });
@@ -36,5 +72,6 @@ router.get('/:id', function(req, res) {
     res.status(400).render('main/404');
   });
 });
+
 
 module.exports = router;
--- index.js
+++ index.js
@@ -6,18 +6,18 @@ var app = express();
 
 app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(ejsLayouts);
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
   db.project.findAll()
-  .then(function(projects) {
-    res.render('main/index', { projects: projects });
-  })
-  .catch(function(error) {
+  .then(function(projects){
+    res.render('main/index', {projects: projects});
+  }).catch(function(error) {
     res.status(400).render('main/404');
   });
 });
+
 
 app.use('/projects', require('./controllers/projects'));
 
