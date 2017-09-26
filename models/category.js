0b77f49a9f02252affd199660cc27ed0eecd2852
Elizabeth friedland <elizabethfriedland@Elizabeths-MacBook.local>
2017-09-26 16:48:31 -0700

    created category migration and intermediate model

---
+++ models/category.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var category = sequelize.define('category', {
+    name: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        models.category.belongsToMany(models.project, {through: "categoriesProjects"});
+      }
+    }
+  });
+  return category;
+};
