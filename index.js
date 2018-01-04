73f30b1be4fa25eb87f1824960d05a9616d9b4a4
Nick Nedev <nedevn@gmail.com>
2018-01-03 20:26:02 -0800

    Creates models for cateroy and the join table. Posting new projects not working

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/', function(req, res) {
 
 app.use('/projects', require('./controllers/projects'));
 
-var server = app.listen(process.env.PORT || 3000, function() {
+var server = app.listen(process.env.PORT || 3003, function() {
   rowdy.print();
 });
 
