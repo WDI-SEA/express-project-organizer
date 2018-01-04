bc5fd41c7f7b601a2f77921f1b24f631e6eaf20b
aishabraud92 <aishabraud@gmail.com>
2018-01-03 19:41:13 -0800

    project add

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var db = require('./models');
-var rowdy = require('rowdy-logger');
 var app = express();
-
-rowdy.begin(app);
 
 app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
@@ -13,19 +10,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
-  db.project.findAll()
+    db.project.findAll()
         .then(function(projects) {
-          res.render('main/index', { projects: projects });
+            res.render('main/index', { projects: projects });
         })
         .catch(function(error) {
-          res.status(400).render('main/404');
+            res.status(400).render('main/404');
         });
 });
 
 app.use('/projects', require('./controllers/projects'));
+app.use('/categories', require('./controllers/categories'));
 
-var server = app.listen(process.env.PORT || 3000, function() {
-  rowdy.print();
-});
+var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
