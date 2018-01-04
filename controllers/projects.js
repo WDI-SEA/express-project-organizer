02127f324ee668797cdb1244a6f3d1124700d9cb
RAnderso0325 <reillyjanderson@gmail.com>
2018-01-03 21:30:52 -0800

    post new article with categories works

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,17 +4,36 @@ var db = require('../models');
 var router = express.Router();
 
 // POST /projects - create a new project
+
 router.post('/', function(req, res) {
+  var categories = [];
+  if(req.body.categories){
+    categories = req.body.categories.split(',');
+    console.log(categories);
+  }
   db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
     deployedLink: req.body.deployedLink,
     description: req.body.description
-  })
-  .then(function(project) {
-    res.redirect('/');
-  })
-  .catch(function(error) {
+  }).then(function(createdProject){
+    if(categories.length > 0){
+      async.forEach(categories,function(c,callback){
+        db.category.findOrCreate({
+          where: {content: c.trim()}
+        }).spread(function(category,wasCreated){
+          if(category){
+            createdProject.addCategory(category);
+          }
+          callback(null);
+        });
+      }, function(){
+        res.redirect('/');
+      });
+    }else{
+      res.redirect('/');
+    }
+  }).catch(function(error) {
     res.status(400).render('main/404');
   });
 });
@@ -32,7 +51,7 @@ router.get('/:id', function(req, res) {
   })
   .then(function(project) {
     if (!project) throw Error();
-    res.render('projects/show', { project: project });
+    res.render('projects/show', { result: project });
   })
   .catch(function(error) {
     res.status(400).render('main/404');
--- index.js
+++ index.js
@@ -10,21 +10,21 @@ rowdy.begin(app);
 app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public/'));
 app.use(ejsLayouts);
-
-app.get('/', function(req, res) {
-  db.project.findAll()
-        .then(function(projects) {
-          res.render('main/index', { projects: projects });
-        })
-        .catch(function(error) {
-          res.status(400).render('main/404');
-        });
-});
 
 app.use('/projects', require('./controllers/projects'));
 app.use('/categories', require('./controllers/categories'));
 
+app.get('/', function(req, res) {
+  db.project.findAll({
+  	include: [db.category]
+  }).then(function(projects) {
+  	res.render('main/index', { projects: projects });
+  }).catch(function(error) {
+    res.status(400).render('main/404');
+  });
+});
 var server = app.listen(process.env.PORT || 3000, function() {
   rowdy.print();
 });
