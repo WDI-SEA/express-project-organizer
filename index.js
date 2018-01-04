b9db238308f27806ca3e1ca1b085114e1462ee18
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-01-04 10:56:50 -0800

    Finished, though Part IV already Bootstrapped.

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.get('/', function(req, res) {
 
 app.use('/projects', require('./controllers/projects'));
 app.use('/categories', require('./controllers/categories'));
+app.use(express.static(__dirname + '/public'));
 
 var server = app.listen(process.env.PORT || 3000, function() {
   rowdy.print();
