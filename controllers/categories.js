49cefafdfb0be99d0288ece3e45b165b8fce9a16
RAnderso0325 <reillyjanderson@gmail.com>
2018-01-03 21:41:55 -0800

    post route for categories is working and category single pages is working with articles

--- controllers/categories.js
+++ controllers/categories.js
@@ -8,7 +8,15 @@ router.get('/', function(req,res){
 });
 
 router.get('/:id', function(req,res){
-	res.send('get route for individual page reached');
+	db.category.findOne({
+		where: {id: req.params.id},
+		include: [db.project]
+	}).then(function(category){
+		if(!category) throw Error();
+		res.render('categories/show', {result: category});
+	}).catch(function(error){
+		res.status(400).render('main/404');
+	});
 });
 
 router.delete('/:id', function(req,res){
