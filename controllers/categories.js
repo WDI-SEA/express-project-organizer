e063376fbe9adcaa9482087d57721f0f55163862
TaylorDarneille <tmdarneille@dons.usfca.edu>
2018-01-05 08:57:10 -0800

    submission

---
+++ controllers/categories.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models');
+
+router.get('/', function(req, res){
+  db.category.findAll({
+    include: [db.project]
+  }).then(function(categories){
+    res.render('categories/index.ejs', {categories: categories});
+  });
+});
+
+router.get('/:id', function(req, res){
+	db.category.findOne({
+		where: {id: req.params.id},
+		include: [db.project]
+	}).then(function(category){
+		res.render('categories/single', {category: category});
+	});
+});
+
+module.exports = router;
--- controllers/projects.js
+++ controllers/projects.js
@@ -1,22 +1,52 @@
 var express = require('express');
-var db = require('../models');
+var async = require('async');
 var router = express.Router();
+var db = require('../models');
 
-// POST /projects - create a new project
 router.post('/', function(req, res) {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployedLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then(function(project) {
-    res.redirect('/');
-  })
-  .catch(function(error) {
-    res.status(400).render('main/404');
-  });
-});
+  console.log()
+  db.project.create(req.body).then(function(createdProject){
+    //new code
+    if(!req.body.categories){
+      console.log("no categories added");
+      res.redirect('/projects/'+createdProject.id);
+    }
+    else {
+      //Will need to make a hook to avoid adding empty tags,
+      //but this will trim the whitespace around each tag,
+      //and put them into an array called categories
+      var categories = req.body.categories.split(',');
+      //removes any whitespace elements from the array
+      categories = categories.filter(function(category) { return /\S/.test(category); });
+      categories.forEach(function(item, index, arr){
+        arr[index] = item.trim();
+      });
+      //add categories to table
+      async.forEach(categories, function(c, callback){
+        db.category.findOrCreate({
+          where: {name: c}
+        })//end of findOrCreate
+        .spread(function(cat, wasCreated){
+          if(cat){
+            createdProject.addCategory(cat);
+          }//end of if(cat){
+          //calling callback(null) is saying everything is done
+          callback(null);
+        })//end of spread(function(cat, wasCreated){
+      },
+      //Third argument of the async.forEach()
+      //happens when ALL the calls have been resovled
+      function(){ 
+        res.redirect('/projects/'+createdProject.id);
+      });//end of async.forEach(
+    }//end of else
+    //end of new code
+  })//end of db.project.create(req.body)
+  .catch(function(err){
+    console.log('err', err);
+    res.send('uh oh!');
+  }); //end of catch
+});//end of route
 
 // GET /projects/new - display form for creating a new project
 router.get('/new', function(req, res) {
@@ -26,7 +56,8 @@ router.get('/new', function(req, res) {
 // GET /projects/:id - display a specific project
 router.get('/:id', function(req, res) {
   db.project.find({
-    where: { id: req.params.id }
+    where: { id: req.params.id },
+    include: [db.category]
   })
   .then(function(project) {
     if (!project) throw Error();
