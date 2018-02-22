841b32730e25654ebcb48cb6c9a25174677e8498
hannan-emily <emilyhannan@gmail.com>
2018-02-21 22:12:41 -0800

    structure for posting new category in place, but will not resolve an error when submit button is clicked

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var db = require('../models');
 var router = express.Router();
+var bodyParser = require('body-parser')
+
+// +router.get('/', function(req, res){
+// +  db.category.findAll().then(function(categories) {
+// +    res.render('categories/index', {categories: categories});
+// +    }).catch(function(error) {
+// +      res.status(400).render('main/404');
+// +  });
+// +});
+// +router.get('/:id', function(req, res) {
+// +  db.category.findOne({
+// +    where: {id: req.params.id},
+// +    include: [db.project]
+// +  }).then(function(category){
+// +    res.render('categories/show', {projects: projects, category: category});
+// +    }).catch(function(error) {
+// +      res.status(400).render('main/404');
+// +    });
+// +});
+// +module.exports = router;
 
 // POST /categories - create a new category
 router.post('/', function(req, res) {
   db.category.create({
     name: req.body.name,
-    description: req.body.description
   })
-  .then(function(project) {
+  .then(function(category) {
     res.redirect('/');
   })
   .catch(function(error) {
@@ -23,8 +42,11 @@ router.get('/new', function(req, res) {
 
 //GET /categories
 router.get('/', function(req, res) {
-  res.render('categories');
-})
+  db.category.findAll().then(function(categories) {
+    res.render('categories/show', { categories: categories });
+  });
+});
+
 
 
 module.exports = router;
--- controllers/projects.js
+++ controllers/projects.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var db = require('../models');
+var async = require('async');
 var router = express.Router();
 
 // POST /projects - create a new project
+// add in new category using asyc - pulling from express-blogpulse project
+// POST /projects - create a new project
 router.post('/', function(req, res) {
+  var categories = [];
+  if (req.body.categories) {
+    categories = req.body.categories.split(',');
+  }
   db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
@@ -11,12 +18,33 @@ router.post('/', function(req, res) {
     description: req.body.description
   })
   .then(function(project) {
-    res.redirect('/');
+
+    if (categories.length > 0) {
+      async.forEach(categories, function(cat, callback) {
+        // This is the iterator function
+        // Add the tag to the tags table
+        db.category.findOrCreate({
+          where: {name: cat.trim()}
+        }).spread(function(newCat, wasCreated) {
+          project.addCategory(newCat).then(function() {
+            callback();
+          });
+        });
+      }, function() {
+        //This is the function that runs when everything is resolved
+        // Redirect to home
+        res.redirect('/');
+      });
+    } else {
+      // if no categories, just redirect
+      res.redirect('/');
+    }
   })
   .catch(function(error) {
     res.status(400).render('main/404');
   });
 });
+
 
 // GET /projects/new - display form for creating a new project
 router.get('/new', function(req, res) {
