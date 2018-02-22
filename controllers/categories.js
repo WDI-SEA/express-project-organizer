12e603c5838b09bcbcca27cf3022c2d498e0d31f
hasstMac <hasstmac@hasstMacs-MacBook-Air.local>
2018-02-22 09:54:32 -0800

    mostly done, some of the relationships are not established, but you can add categories and associate with new projects.

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var db = require('../models');
 var router = express.Router();
+var async = require('async');
 
 // GET /categories - display form to create new category
 router.get('/', function(req, res){
@@ -10,8 +11,31 @@ router.get('/', function(req, res){
 });
 
 // GET /categories/:id - display list of projects by category
+router.get('/categories/:id', function(req, res){
+  console.log('in category/id path');
+  db.category.find({
+    where: {id:req.params.id},
+    include: [db.project]
+  })
+  .then(function(category){
+    if (!category) throw Error();
+    res.render('categories/new', {category:category});
+  })
+  .catch(function(error){
+    res.status(400).render('main/404');
+  });
+});
 
+// EDIT /categories/:id
+router.put('/categories/:id', function(req,res){
+  console.log('in category/id PUT path');
 
+});
 
+// DELETE /categories/:id
+router.delete('/categories/:id', function(req,res){
+  console.log('in category/id DELETE path');
+
+});
 
 module.exports = router;
