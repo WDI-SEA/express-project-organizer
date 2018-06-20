8a20ae6313398224d0b53384e26028e2c774b547
shullmb <shullmb@gmail.com>
2018-06-19 20:33:04 -0700

    added categories index route and view

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,5 +2,12 @@ const express = require('express');
 const db = require('../models');
 const router = express.Router();
 
+// GET - categories index
+router.get('/', (req,res) => {
+    db.category.findAll().then((categories) => {
+        res.render('categories/index', {categories});
+    })
+})
+
 
 module.exports = router;
