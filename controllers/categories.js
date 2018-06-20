7b399d7766ba5ce5fe4552a683350d76407be71d
William Connelly <williamconnelly.dev@gmail.com>
2018-06-19 22:10:37 -0700

    Added index and show pages for categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,6 +2,19 @@ var express = require('express');
 var db = require('../models');
 var router = express.Router();
 
+// GET - /categories - gets all categories
+router.get("/", function(req, res) {
+	db.category.findAll().then(function(data) {
+		res.render("categories/index", {categories: data});
+	});
+});
 
+router.get("/:id", function(req, res) {
+	db.category.findById(req.params.id).then(function(category) {
+		category.getProjects().then(function(projects) {
+			res.render("categories/show", {category: category, projects: projects});
+		});
+	});
+});
 
 module.exports = router;
