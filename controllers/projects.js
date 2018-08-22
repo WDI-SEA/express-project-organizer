46ebb9e1835e28b144dadf5cea4b9f4dcc29d107
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-22 08:16:01 -0700

    add categories to POST project

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,6 +4,11 @@ var router = express.Router();
 
 // POST /projects - create a new project
 router.post('/', function(req, res) {
+  console.log('name sent:', req.body.name);
+  console.log('github:', req.body.githubLink);
+  console.log('deploy:', req.body.deployedLink);
+  console.log('categories sent:', req.body.categories);
+  console.log('desc:', req.body.description);
   db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
@@ -11,6 +16,20 @@ router.post('/', function(req, res) {
     description: req.body.description
   })
   .then(function(project) {
+    console.log('reached start of promise for new proj');
+    // do category stuff
+    var categories = [];
+    if (req.body.categories) {
+      console.log('category field is not empty');
+      categories = req.body.categories.split(' ');
+    }
+    categories.forEach(function(cat) {
+      db.category.findOrCreate({
+        where: { name: cat.trim() }
+      }).spread(function(newCat, wasCreated) {
+        project.addCategory(newCat);
+      });
+    });
     res.redirect('/');
   })
   .catch(function(error) {
