507734112c2cf2001e17df92ac4c792f73c70fef
idothestamping <douglas.klemp@gmail.com>
2019-01-10 01:54:09 -0800

    "Deliverable Complete"

---
+++ controllers/categories.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var db = require('../models');
+var router = express.Router();
+
+router.get('/', function(req, res){
+	db.category.findAll()
+	.then(function(categories){
+		res.render('categories/index', { categories: categories });
+	})
+	.catch(function(err){
+		console.log(err);
+		res.render('main/404');
+	})
+});
+
+router.get('/:id', function(req, res){
+	db.category.findOne({
+		where: { id: req.params.id },
+		include: [db.project]
+	})
+	.then(function(category){
+		res.render('categories/show', { category: category });
+	})
+	.catch(function(err){
+		console.log(err);
+		res.render('main/404');
+	})
+})
+
+module.exports = router; 
--- controllers/projects.js
+++ controllers/projects.js
@@ -1,22 +1,54 @@
+var async = require('async');
 var express = require('express');
 var db = require('../models');
 var router = express.Router();
 
-// POST /projects - create a new project
-router.post('/', function(req, res) {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployedLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then(function(project) {
-    res.redirect('/');
-  })
-  .catch(function(error) {
-    res.status(400).render('main/404');
+// // POST /projects - create a new project
+// router.post('/', function(req, res) {
+//   db.project.create({
+//     name: req.body.name,
+//     githubLink: req.body.githubLink,
+//     deployedLink: req.body.deployedLink,
+//     description: req.body.description
+//   })
+//   .then(function(project) {
+//     res.redirect('/');
+//   })
+//   .catch(function(error) {
+//     res.status(400).render('main/404');
+//   });
+// });
+
+router.post('/', function(req, res){
+  db.project.create(req.body).then(function(newProject){
+    var categories = [];
+    if (req.body.category){
+      categories = req.body.category.split(",")
+    }
+    if(categories.length>0){
+      async.forEach(categories, function(t, done){
+        db.category.findOrCreate({
+          where: {name: t.trim()}
+        }).spread(function(newCategory, wasCreated){
+            newProject.addCategory(newCategory).then(function(){
+            done();// tell async, this iteration is all finished
+            })
+            .catch(done);
+        })
+        .catch(done);
+      }, function(){
+      res.redirect('/projects/' + newProject.id)
+      })
+    } else {
+      res.redirect('/projects/new' + newProject.id);
+      }
+    })
+    .catch(function(error){
+      res.status(400).render('main/404');
+    });
   });
-});
+
+
 
 // GET /projects/new - display form for creating a new project
 router.get('/new', function(req, res) {
@@ -26,9 +58,11 @@ router.get('/new', function(req, res) {
 // GET /projects/:id - display a specific project
 router.get('/:id', function(req, res) {
   db.project.find({
-    where: { id: req.params.id }
+    where: { id: req.params.id },
+    include: [db.category] // new assoicated table
   })
   .then(function(project) {
+    console.log(project);
     if (!project) throw Error();
     res.render('projects/show', { project: project });
   })
