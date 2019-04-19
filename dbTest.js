386dbc0fdef7fc832edac52cd0738359e53cf88e
brandiw <brandimichelle.0911@gmail.com>
2019-04-18 17:00:53 -0700

    Adding async forEach example

--- dbTest.js
+++ dbTest.js
@@ -1,5 +1,5 @@
 var db = require('./models')
-
+var async = require('async')
 // Create a category: Category model must exist and be migrated
 
 // db.category.create({
@@ -11,15 +11,48 @@ var db = require('./models')
 // Create a project and use the helper function create<ModelName> to create a category
 // Requires categoriesProjects to exist, be migrated, and properly associated
 
-db.project.findOne({
-  where: { id: 1 },
-  include: [db.category]
-}).then(function(project) {
-  // by using eager loading, the project model should have a categories key
-  console.log(project.categories);
+var cats = ['node', 'javascript', 'react', 'css', 'html']
 
-  // a createCategory function should be available to this model
-  project.createCategory({ name: 'node' }).then(function(category) {
-    console.log(category.get());
-  });
-});
+db.project.create({
+  name: 'PROJECT TWO',
+  deployLink: 'http://github.com/brandiw',
+  githubLink: 'http://github.com/brandiw',
+  description: 'This was a game'
+}).then(function(project) {
+  // IMPROVED VERSION WITH ASYNC
+  // async.forEach(arrayToIterate, iteratorFunctionToRunOnEachItem(item, callback), functionToRunWhenAllComplete)
+  async.forEach(cats, (cat, done) => {
+    db.category.findOrCreate({
+      where: { name: cat }
+    })
+    .spread((category, wasCreated) => {
+      project.addCategory(category)
+      .then(() => {
+        // res.redirect, or whatevs
+        console.log('done adding', cat)
+        done()
+      })
+    })
+  }, () => {
+    console.log('EVERYTHING is done. Now redirect or something')
+  })
+
+
+
+
+
+  // TIMING DOESNOT WORK
+  // cats.forEach((cat) => {
+  //   db.category.findOrCreate({
+  //     where: { name: cat }
+  //   })
+  //   .spread((category, wasCreated) => {
+  //     project.addCategory(category)
+  //     .then(() => {
+  //       // res.redirect, or whatevs
+  //       console.log('done adding', cat)
+  //     })
+  //   })
+  // })
+  // console.log('redirect or something')
+})
