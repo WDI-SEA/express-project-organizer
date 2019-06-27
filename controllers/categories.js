604e2fbe036cc78b37badc7603e76ab2f7c80e4d
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-06-26 22:40:38 -0700

    tried adding GET /categories and GET /categories/:id

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,8 +2,41 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-// GET /tags - index - show ALL the tags
+// GET /categories - show ALL the categories that exist
+router.get('/', function(req, res) {
+  db.category.findAll({
+    include: [db.project],
+    through: {
+      attributes: [projectId],
+      where: {completed: true}
+    }
+  }).then(function(categories) {
+    console.log(categories);
+    res.render('authors/index', {categories});
+  });
+});
+// router.get('/', function(req, res) {
+//   db.category.find({
+//     where: {name: req.body.name}
+//   }).then(function(projects) {
+//     projects.forEach(function(project) {
+//       return project.name;
+//     })
+//     res.render('categories/index', {categories});
+//   });
+// });
 
+//GET /categories/:id - show a specific category and all the projects with that category
+db.project.find({
+  where: {name: req.param.name}
+}).then(function(project) {
+  project.getCategories().then(function(categories) {
+    console.log("These categories are tagged with " + project.name + ":");
+    categories.forEach(function(category) {
+      console.log("Post title: " + category.name);
+    });
+  });
+});
 
 // POST /tags - post the tags
 // router.post('/', function(req, res) {
@@ -22,6 +55,6 @@ const db = require('../models');
 //   })
 // });
 
-//GET /tags/:id - show ONE tag and its associated posts
+
 
 module.exports = router;
