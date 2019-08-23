c89b47ad1045e30aa06d3ab0e72863d42ed7ff33
stanstaton <thatkidenzo@gmail.com>
2019-08-22 19:24:31 -0700

    added controller routes and views for category

---
+++ controllers/categories.js
@@ -0,0 +1,28 @@
+let router = require('express').Router();
+let db = require('../models')
+
+router.get('/', (req,res) => {
+    db.category.findAll()
+    .then(category => {
+        res.render('categories/index', {category})
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('whoops')
+    })
+    
+})
+
+router.get('/:id', (req,res) => {
+    db.category.findByPk(req.params.id, {include: [db.project]})
+    .then(category => {
+        res.render('categories/show', {category})
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('Whoops')
+    })
+})
+
+
+module.exports = router;
