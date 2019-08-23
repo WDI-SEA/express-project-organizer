9cd45213bbbfdf78bb300df6348396333933ce5b
stanstaton <thatkidenzo@gmail.com>
2019-08-22 19:02:13 -0700

    models created

--- dbTest.js
+++ dbTest.js
@@ -1,5 +1,5 @@
 var db = require('./models')
-var async = require('async')
+// var async = require('async')
 // Create a category: Category model must exist and be migrated
 
 // db.category.create({
@@ -11,33 +11,39 @@ var async = require('async')
 // Create a project and use the helper function create<ModelName> to create a category
 // Requires categoriesProjects to exist, be migrated, and properly associated
 
-var cats = ['node', 'javascript', 'react', 'css', 'html']
+// var cats = ['node', 'javascript', 'react', 'css', 'html']
 
-db.project.create({
-  name: 'PROJECT TWO',
-  deployLink: 'http://github.com/brandiw',
-  githubLink: 'http://github.com/brandiw',
-  description: 'This was a game'
-}).then(function(project) {
-  // IMPROVED VERSION WITH ASYNC
-  // async.forEach(arrayToIterate, iteratorFunctionToRunOnEachItem(item, callback), functionToRunWhenAllComplete)
-  async.forEach(cats, (cat, done) => {
-    db.category.findOrCreate({
-      where: { name: cat }
-    })
-    .spread((category, wasCreated) => {
-      project.addCategory(category)
-      .then(() => {
-        // res.redirect, or whatevs
-        console.log('done adding', cat)
-        done()
-      })
-    })
-  }, () => {
-    console.log('EVERYTHING is done. Now redirect or something')
-  })
+// db.project.create({
+//   name: 'PROJECT TWO',
+//   deployLink: 'http://github.com/brandiw',
+//   githubLink: 'http://github.com/brandiw',
+//   description: 'This was a game'
+// }).then(function(project) {
+//   // IMPROVED VERSION WITH ASYNC
+//   // async.forEach(arrayToIterate, iteratorFunctionToRunOnEachItem(item, callback), functionToRunWhenAllComplete)
+//   async.forEach(cats, (cat, done) => {
+//     db.category.findOrCreate({
+//       where: { name: cat }
+//     })
+//     .spread((category, wasCreated) => {
+//       project.addCategory(category)
+//       .then(() => {
+//         // res.redirect, or whatevs
+//         console.log('done adding', cat)
+//         done()
+//       })
+//     })
+//   }, () => {
+//     console.log('EVERYTHING is done. Now redirect or something')
+//   })
 
+// var db = require('./models')
 
+db.category.create({
+  name: 'node'
+}).then(function(category) {
+  console.log(category.get())
+})
 
 
 
@@ -55,4 +61,4 @@ db.project.create({
   //   })
   // })
   // console.log('redirect or something')
-})
+// })
---
+++ models/category.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define('category', {
+    name: DataTypes.STRING
+  }, {});
+  category.associate = function(models) {
+    // associations can be defined here
+  };
+  return category;
+};
