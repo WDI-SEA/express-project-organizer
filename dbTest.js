4a16e0b34df7d6d95408c4f73556373d4bc6bc7c
nickubed <nhpquandt@gmail.com>
2019-12-27 13:08:21 -0800

    Initial setup complete

--- dbTest.js
+++ dbTest.js
@@ -11,33 +11,46 @@ var async = require('async')
 // Create a project and use the helper function create<ModelName> to create a category
 // Requires categoriesProjects to exist, be migrated, and properly associated
 
-var cats = ['node', 'javascript', 'react', 'css', 'html']
+// var cats = ['node', 'javascript', 'react', 'css', 'html']
 
-db.project.create({
-  name: 'PROJECT TWO',
-  deployLink: 'http://github.com/brandiw',
-  githubLink: 'http://github.com/brandiw',
-  description: 'This was a game'
-}).then(function(project) {
-  // IMPROVED VERSION WITH ASYNC
-  // async.forEach(arrayToIterate, iteratorFunctionToRunOnEachItem(item, callback), functionToRunWhenAllComplete)
-  async.forEach(cats, (cat, done) => {
-    db.category.findOrCreate({
-      where: { name: cat }
-    })
-    .spread((category, wasCreated) => {
-      project.addCategory(category)
-      .then(() => {
-        // res.redirect, or whatevs
-        console.log('done adding', cat)
-        done()
-      })
-    })
-  }, () => {
-    console.log('EVERYTHING is done. Now redirect or something')
+// db.project.create({
+//   name: 'PROJECT TWO',
+//   deployLink: 'http://github.com/brandiw',
+//   githubLink: 'http://github.com/brandiw',
+//   description: 'This was a game'
+// }).then(function(project) {
+//   // IMPROVED VERSION WITH ASYNC
+//   // async.forEach(arrayToIterate, iteratorFunctionToRunOnEachItem(item, callback), functionToRunWhenAllComplete)
+//   async.forEach(cats, (cat, done) => {
+//     db.category.findOrCreate({
+//       where: { name: cat }
+//     })
+//     .spread((category, wasCreated) => {
+//       project.addCategory(category)
+//       .then(() => {
+//         // res.redirect, or whatevs
+//         console.log('done adding', cat)
+//         done()
+//       })
+//     })
+//   }, () => {
+//     console.log('EVERYTHING is done. Now redirect or something')
+//   })
+
+  var db = require('./models')
+
+  db.project.findOne({
+    where: { id: 1 },
+    include: [db.category]
+  }).then(function(project) {
+    // by using eager loading, the project model should have a categories key
+    console.log(project.categories)
+  
+    // a createCategory function should be available to this model
+    // project.createCategory({ name: 'node' }).then(function(category) {
+    //   console.log(category.get())
+    // })
   })
-
-
 
 
 
@@ -55,4 +68,4 @@ db.project.create({
   //   })
   // })
   // console.log('redirect or something')
-})
+// })
---
+++ models/category.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const category = sequelize.define('category', {
+    name: DataTypes.STRING
+  }, {});
+  category.associate = function(models) {
+    // associations can be defined here
+    models.category.belongsToMany(models.project, { through: 'categoriesProjects' })
+
+  };
+  return category;
+};
