71990272b38f89c89ca176bc9a20780df1fe9484
royce-u <royceubando@gmail.com>
2020-04-23 13:44:54 -0700

    tag working

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,21 +1,45 @@
 let express = require('express')
 let db = require('../models')
 let router = express.Router()
+let async = require('async')
 
 // POST /projects - create a new project
 router.post('/', (req, res) => {
+  let categories = []
+  if (req.body.categoryName) {
+    categories = req.body.categoryName.split(',')
+  }
   db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
     deployLink: req.body.deployedLink,
     description: req.body.description
   })
-  .then((project) => {
-    res.redirect('/')
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+    .then((project) => {
+      async.forEach(categories, (cat, done) => {
+        db.category.findOrCreate({
+          where: { name: cat.trim() }
+          })
+          .then(([cat, wasCreated]) => {
+            project.addCategory(cat)
+            .then(() => {
+              done()
+            })
+            .catch(err => {
+              console.log('error')
+              done()
+            })
+          })
+          .catch(err => {
+            console.log('error')
+            done()
+            res.status(400).render('main/404')
+          })
+      }),
+      () => {
+        res.redirect('/')
+      }
+    })
 })
 
 // GET /projects/new - display form for creating a new project
@@ -26,15 +50,19 @@ router.get('/new', (req, res) => {
 // GET /projects/:id - display a specific project
 router.get('/:id', (req, res) => {
   db.project.findOne({
-    where: { id: req.params.id }
+    where: { id: req.params.id },
+    include: [db.category]
   })
-  .then((project) => {
-    if (!project) throw Error()
-    res.render('projects/show', { project: project })
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+    .then((project) => {
+      // console.log('project categories-------' project.categories)
+      if (!project) throw Error()
+
+      res.render('projects/show', { project: project })
+    })
+    .catch((error) => {
+      console.log('did you throw an error?---------------', error)
+      res.status(400).render('main/404')
+    })
 })
 
 module.exports = router
