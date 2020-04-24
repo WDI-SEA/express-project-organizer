64167b9782ce56c381524b3a143539a845f6cf10
Jummay77 <hansangho@yahoo.com>
2020-04-23 18:59:37 -0700

    updated associations and POST route works

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,21 +1,49 @@
 let express = require('express')
 let db = require('../models')
 let router = express.Router()
+let async = require('async')
 
 // POST /projects - create a new project
 router.post('/', (req, res) => {
+  let categories = []
+  if (req.body.categoryName) {
+    categories = req.body.categoryName.split(',')
+  }
   db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
     deployLink: req.body.deployedLink,
     description: req.body.description
   })
-  .then((project) => {
-    res.redirect('/')
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+    .then((project) => {
+      async.forEach(categories, (category, done) => {
+        db.category.findOrCreate({
+          where: {
+            name: category.trim()
+          }
+        })
+          .then(([category, wasCreated]) => {
+            project.addCategory(category)
+              .then(() => {
+                done()
+              })
+          })
+          .catch((err) => {
+            console.log(error)
+            done()
+            // res.status(400).render('main/404')
+          })
+          .catch((err) => {
+            console.log(error)
+            done()
+            res.status(400).render('main/404')
+          })
+      }, 
+      () => {
+        res.redirect('/')
+      }) // End of async function
+    })  // End of then for the project create function
+
 })
 
 // GET /projects/new - display form for creating a new project
@@ -26,15 +54,18 @@ router.get('/new', (req, res) => {
 // GET /projects/:id - display a specific project
 router.get('/:id', (req, res) => {
   db.project.findOne({
-    where: { id: req.params.id }
+    where: { id: req.params.id },
+    include: [db.category]
   })
-  .then((project) => {
-    if (!project) throw Error()
-    res.render('projects/show', { project: project })
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+    .then((project) => {
+      console.log(project.categories)
+      if (!project) throw Error()
+      res.render('projects/show', { project: project })
+    })
+    .catch((error) => {
+      console.log('THIS IS AN ERROR BRAH', error)
+      res.status(400).render('main/404')
+    })
 })
 
 module.exports = router
