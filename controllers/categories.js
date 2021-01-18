6b3a85f0cb3ae2103f64185d8acfec1189859f84
ElyssaW <ejwinch1@gmail.com>
2021-01-18 01:16:32 -0600

    Added delete button to categories

--- controllers/categories.js
+++ controllers/categories.js
@@ -7,14 +7,20 @@ router.get('/', (req, res) => {
   db.category.findAll({
         include: [db.project]
     }).then(categories => {
-        categories.forEach(category => {
-            
-        })
         console.log(categories)
         res.render('categories/index', {categories: categories})
     }).catch((error) => {
         res.status(400).render('main/404')
       })
+})
+
+//Route to delete projects
+router.delete('/delete/:id', (req, res) => {
+    db.category.destroy({
+      where: {id: req.params.id}
+    }).then(() => {
+      res.redirect('/categories/')
+    })
 })
 
 // Route to get one category and all associated projects
@@ -24,8 +30,6 @@ router.get('/:name', (req, res) => {
         where: {name: req.params.name}
     }).then(category => {
         category.getProjects().then(projects => {
-            console.log(category)
-            console.log(projects)
             res.render('categories/show', {category: category, projects: projects})
         })
     })
