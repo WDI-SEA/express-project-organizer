ea943ba939b6a7c8e0cb88857229e2572fe7c16c
Blake DeGraw <bbdegraw@gmail.com>
2021-01-17 16:25:25 -0800

    associations defined

--- dbTest.js
+++ dbTest.js
@@ -1,5 +1,18 @@
 const db = require('./models')
 
-db.category.create({
-    name: 'node'
-})
+// db.category.create({
+//     name: 'node'
+// })
+
+db.project.findOne({
+    where: { id: 1 },
+    include: [db.category]
+  }).then(project => {
+    // by using eager loading, the project model should have a categories key
+    console.log(project.categories)
+  
+    // createCategory function should be available to this model - it will create the category then add it to the project
+    project.createCategory({ name: 'express' }).then(category => {
+      console.log(category.id)
+    })
+  })
