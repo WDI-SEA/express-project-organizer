e2e0da818cfac61b232c504e9c0dc6856bfaab50
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2021-01-19 12:04:12 -0800

    commented out test

--- dbTest.js
+++ dbTest.js
@@ -1,7 +1,7 @@
-const db = require('./models')
+// const db = require('./models')
 
-db.category.create({
-  name: 'node'
-}).then(category => {
-  console.log(category.id)
-})
+// db.category.create({
+//   name: 'node'
+// }).then(category => {
+//   console.log(category.id)
+// })
