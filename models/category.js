b096c4a04213dabb50ad0754698cf1805a4cfdb5
Jackson Franks <jacksonfranks@Jacksons-Air.lan>
2021-01-19 16:57:04 -0600

    all done.. i think?

--- models/category.js
+++ models/category.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.category.belongsToMany(models.project, {through: 'categoriesProjects'})
     }
   };
   category.init({
