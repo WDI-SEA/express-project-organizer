658d238e9045b3226d160de090b3df27e5015d4b
Blake DeGraw <bbdegraw@gmail.com>
2021-01-18 20:15:52 -0800

    routine push

--- controllers/projects.js
+++ controllers/projects.js
@@ -9,16 +9,16 @@ router.post('/', (req, res) => {
         githubLink: req.body.githubLink,
         deployLink: req.body.deployedLink,
         description: req.body.description
-  }).then((project)=> {
+  }).then(project => {
     db.category.findOrCreate({
         where: {
         name: req.body.category
         }
     })
-    .then((category)=>{
+    .then(category => {
       project.addCategory(category)
     })
-  }).then((project) => {
+  }).then(project => {
     res.redirect('/')
   })
   .catch((error) => {
