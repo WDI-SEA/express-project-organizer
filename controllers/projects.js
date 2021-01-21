b9b9f4ccc91cd9c8082ccb900fa00d04af53bf20
Ilka Santiago <home@Ilkas-MacBook-Pro.local>
2021-01-20 23:16:50 -0800

    assignment

--- controllers/projects.js
+++ controllers/projects.js
@@ -81,33 +81,38 @@ router.delete("/:id", (req,res)=>{
 
 // To edit a specific project
 router.put("/:id", (req,res)=>{
-  db.project.update({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployLink: req.body.deployedLink,
-    description: req.body.description},
-    {
-      where: {id: req.params.id}
+  // Start by finding the project
+  db.project.findOne({
+    where: {id: req.params.id},
+    include: [db.category]    
   }).then(project =>{
-    let str = req.body.categoryName
-    str = str.split(",") 
-    console.log(str)
-    for(let i=0; i<str.length; i++){
-      db.category.findOrCreate({
-        where: {
-          name: str[i]
-        }
-      }).then(([category, created]) => {
-        project.addCategory(category).then(relationInfo =>{
-           console.log("Project-Category relation created")
-        })  
+    db.project.update(
+      {
+        name: req.body.name,
+        githubLink: req.body.githubLink,
+        deployLink: req.body.deployedLink,
+        description: req.body.description
+      },
+      {where: {id: req.params.id}}
+    ).then(()=>{
+      let str = req.body.categoryName.split(',')
+      str.forEach(s =>{
+        db.category.findOrCreate({
+          where: {
+            name: s.trim()
+          }
+        }).then(([category, created]) => {
+          project.addCategory(category).then(relationInfo =>{
+             console.log("Project-Category relation created")
+          })  
+        })
       })
-    }
-    res.redirect('/')
+      res.redirect(`/projects/${req.params.id}`)
     })
   .catch((error) => {
     res.status(400).render('main/404')
   })
+})
 })
 
 module.exports = router
