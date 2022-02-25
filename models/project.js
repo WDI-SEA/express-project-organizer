b2c3a3f71a530fc23e95ab70cf30d0f20ae20f5c
zbrose <zrbrose@gmail.com>
2022-02-24 19:23:26 -0800

    models and associations setup

--- models/project.js
+++ models/project.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.project.belongsToMany(models.category,{through: 'categoriesProjects'})
     }
   }
   project.init({
