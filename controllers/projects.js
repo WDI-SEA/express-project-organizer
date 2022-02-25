27a0810f91e28201f0911db209a0aba5ed661780
Norman Teodoro <normanteodoro@hotmail.com>
2022-02-24 22:11:39 -0800

    mvp deliverable

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,21 +1,33 @@
 let express = require('express')
 let db = require('../models')
+const category = require('../models/category')
 let router = express.Router()
 
 // POST /projects - create a new project
-router.post('/', (req, res) => {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then((project) => {
+router.post('/', async (req, res) => {
+  try {
+    const [project, projectCreated] = await db.project.findOrCreate({
+      where: {
+        name: req.body.name,
+        githubLink: req.body.githubLink,
+        deployLink: req.body.deployedLink,
+        description: req.body.description
+      }
+    })
+
+    const [category, categoryCreated] = await db.category.findOrCreate({
+      where: {
+        name: req.body.category
+      }
+    })
+
+    await project.addCategory(category)
+    const getProject = await category.getProjects()
+    const getCategory = await project.getCategories()
     res.redirect('/')
-  })
-  .catch((error) => {
+  } catch (err) {
     res.status(400).render('main/404')
-  })
+  }
 })
 
 // GET /projects/new - display form for creating a new project
@@ -26,15 +38,15 @@ router.get('/new', (req, res) => {
 // GET /projects/:id - display a specific project
 router.get('/:id', (req, res) => {
   db.project.findOne({
-    where: { id: req.params.id }
-  })
-  .then((project) => {
-    if (!project) throw Error()
-    res.render('projects/show', { project: project })
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+      where: { id: req.params.id }
+    })
+    .then(project => {
+      if (!project) throw Error()      
+      res.render('projects/show', { project: project })
+    })
+    .catch(error => {
+      res.status(400).render('main/404')
+    })
 })
 
 module.exports = router
