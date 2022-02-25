c79c56deda0d49a6ef3acd62876c5f32d9b160ab
justin-aquino <justin.aquino25@gmail.com>
2022-02-24 23:25:47 -0800

    able to add new project with category and redirect to homepage

--- controllers/projects.js
+++ controllers/projects.js
@@ -3,19 +3,35 @@ let db = require('../models')
 let router = express.Router()
 
 // POST /projects - create a new project
-router.post('/', (req, res) => {
-  db.project.create({
+// router.post('/', (req, res) => {
+//   db.project.create({
+//     name: req.body.name,
+//     githubLink: req.body.githubLink,
+//     deployLink: req.body.deployedLink,
+//     description: req.body.description
+//   })
+//   .then((project) => {
+//     res.redirect('/')
+//   })
+//   .catch((error) => {
+//     res.status(400).render('main/404')
+//   })
+// })
+
+router.post("/", async (req,res) => {
+  const project = await db.project.create({
     name: req.body.name,
     githubLink: req.body.githubLink,
     deployLink: req.body.deployedLink,
     description: req.body.description
   })
-  .then((project) => {
-    res.redirect('/')
+  const [category, createdCategory] = await db.category.findOrCreate({
+    where: {
+      name:req.body.category
+    }
   })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+  await project.addCategory(category)
+  res.redirect("/")
 })
 
 // GET /projects/new - display form for creating a new project
