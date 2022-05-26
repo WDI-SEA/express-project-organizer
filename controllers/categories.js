619fb00d7cddbf551a5ebbf1f52c81fb3dc4d94a
“erhaneth” <gumusucb21@gmail.com>
2022-05-25 19:16:32 -0700

    first commit

---
+++ controllers/categories.js
@@ -0,0 +1,19 @@
+let express = require('express')
+let db = require('../models')
+let router = express.Router()
+
+router.get('/', async function(req,res){
+ const categories = await db.category.findAll()
+//  console.log(categories)
+ res.render('categories/show.ejs', {categories})
+})
+
+router.get('/:id', async function(req,res){
+ const category = await db.category.finOne()
+ where: {id: req.params.id}
+ imclude: {db.project}
+
+ res.render('categories/assProjects.ejs', {category})
+})
+
+module.exports = router
