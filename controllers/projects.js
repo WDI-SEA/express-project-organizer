10d16dfba503a71d5c2d78aad6bc3d8bab2bbfdc
YOUR-USERNAME <gtellez07@yahoo.com>
2022-12-21 11:35:05 -0600

    first half done

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,19 +4,29 @@ let router = express.Router()
 
 // POST /projects - create a new project
 router.post('/', (req, res) => {
-  db.project.create({
+  db.project.findOrCreate({
+    where: {
     name: req.body.name,
     githubLink: req.body.githubLink,
     deployLink: req.body.deployedLink,
-    description: req.body.description
+    description: req.body.description,
+    }
+  })
+  .then(([project, created]) => {
+    let categoryName = req.body.category.toLowerCase();
+    // console.log(categoryName)
+    db.category.findOrCreate({
+      where: { name: categoryName },
+    });
   })
   .then((project) => {
-    res.redirect('/')
+    res.redirect("/");
   })
   .catch((error) => {
-    res.status(400).render('main/404')
-  })
-})
+    console.log(error);
+    res.status(400).render("main/404");
+  });
+});
 
 // GET /projects/new - display form for creating a new project
 router.get('/new', (req, res) => {
--- models/project.js
+++ models/project.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.project.belongsToMany(models.category, {
+        through: 'categoriesProjects'
+      })
     }
   }
   project.init({
