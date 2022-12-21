91a0a42f0ade3e77716f5e1d3beeedee0448f357
jtw007 <joshwu095@gmail.com>
2022-12-20 19:59:55 -0800

    commit

--- controllers/projects.js
+++ controllers/projects.js
@@ -3,19 +3,28 @@ let db = require('../models')
 let router = express.Router()
 
 // POST /projects - create a new project
-router.post('/', (req, res) => {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then((project) => {
+router.post('/', async (req, res) => {
+  try{
+    const [project, projectCreated] = await db.project.findOrCreate({
+      where:{
+        name: req.body.name,
+        githubLink: req.body.githubLink,
+        deployLink: req.body.deployLink,
+        description: req.body.description
+      }
+    })
+    let categoryName = req.body.category.toLowerCase()
+    const [category, categoryCreated] = await db.category.findOrCreate({
+      where: { 
+        name: categoryName
+      },
+    })
+    await project.addCategory(category)
     res.redirect('/')
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+  } catch(error){
+    console.log(error)
+  }
+  
 })
 
 // GET /projects/new - display form for creating a new project
