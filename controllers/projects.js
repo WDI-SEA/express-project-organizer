f77e6af4cc779f87ba6366456c025c84dba1f60a
tylersb <tylersb@gmail.com>
2022-12-21 17:57:12 -0800

    adds categories

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,21 +1,27 @@
-let express = require('express')
-let db = require('../models')
-let router = express.Router()
+const express = require('express')
+const db = require('../models')
+const router = express.Router()
 
 // POST /projects - create a new project
-router.post('/', (req, res) => {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then((project) => {
+router.post('/', async (req, res) => {
+  try {
+    const project = await db.project.create({
+      name: req.body.name,
+      githubLink: req.body.githubLink,
+      deployLink: req.body.deployedLink,
+      description: req.body.description
+    })
+    const [category] = await db.category.findOrCreate({
+      where: {
+        name: req.body.category
+      }
+    })
+    await category.addProject(project)
     res.redirect('/')
-  })
-  .catch((error) => {
+  } catch (error) {
+    console.log(error)
     res.status(400).render('main/404')
-  })
+  }
 })
 
 // GET /projects/new - display form for creating a new project
@@ -25,16 +31,18 @@ router.get('/new', (req, res) => {
 
 // GET /projects/:id - display a specific project
 router.get('/:id', (req, res) => {
-  db.project.findOne({
-    where: { id: req.params.id }
-  })
-  .then((project) => {
-    if (!project) throw Error()
-    res.render('projects/show', { project: project })
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
+  db.project
+    .findOne({
+      where: { id: req.params.id }
+    })
+    .then((project) => {
+      if (!project) throw Error()
+      res.render('projects/show', { project })
+    })
+    .catch((error) => {
+      console.log(error)
+      res.status(400).render('main/404')
+    })
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require('./models')
 const rowdy = require('rowdy-logger')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 rowdy.begin(app)
 
 app.set('view engine', 'ejs')
@@ -13,14 +13,15 @@ app.use(express.urlencoded({ extended: false }))
 app.use(ejsLayouts)
 
 app.get('/', (req, res) => {
-  db.project.findAll()
-  .then((projects) => {
-    res.render('main/index', { projects: projects })
-  })
-  .catch((error) => {
-    console.log('Error in GET /', error)
-    res.status(400).render('main/404')
-  })
+  db.project
+    .findAll()
+    .then((projects) => {
+      res.render('main/index', { projects })
+    })
+    .catch((error) => {
+      console.log('Error in GET /', error)
+      res.status(400).render('main/404')
+    })
 })
 
 app.use('/projects', require('./controllers/projects'))
@@ -29,7 +30,7 @@ app.get('*', (req, res) => {
   res.render('main/404')
 })
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   rowdy.print()
   console.log(`listening on port: ${PORT}`)
 })
