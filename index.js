f37e58b18c46d47e71914d429cceeded2a241741
Andrew <andrew@andrewsmith.org>
2023-04-06 00:59:31 -0400

    Complete deliverable

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const express = require('express')
-const ejsLayouts = require('express-ejs-layouts')
-const db = require('./models')
-const rowdy = require('rowdy-logger')
+const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+const db = require("./models");
+const rowdy = require("rowdy-logger");
 
-const app = express()
-const PORT = process.env.PORT || 3000
-rowdy.begin(app)
+const app = express();
+const PORT = process.env.PORT || 3000;
+rowdy.begin(app);
 
-app.set('view engine', 'ejs')
-app.use(require('morgan')('dev'))
-app.use(express.urlencoded({ extended: false }))
-app.use(ejsLayouts)
+app.set("view engine", "ejs");
+app.use(require("morgan")("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(ejsLayouts);
 
-app.get('/', (req, res) => {
-  db.project.findAll()
-  .then((projects) => {
-    res.render('main/index', { projects: projects })
-  })
-  .catch((error) => {
-    console.log('Error in GET /', error)
-    res.status(400).render('main/404')
-  })
-})
+app.get("/", (req, res) => {
+  db.project
+    .findAll()
+    .then((projects) => {
+      res.render("main/index", { projects: projects });
+    })
+    .catch((error) => {
+      console.log("Error in GET /", error);
+      res.status(400).render("main/404");
+    });
+});
 
-app.use('/projects', require('./controllers/projects'))
+app.use("/projects", require("./controllers/projects"));
+app.use("/categories", require("./controllers/categories"));
 
-app.get('*', (req, res) => {
-  res.render('main/404')
-})
+app.get("*", (req, res) => {
+  res.render("main/404");
+});
 
-app.listen(PORT, function() {
-  rowdy.print()
-  console.log(`listening on port: ${PORT}`)
-})
+app.listen(PORT, function () {
+  rowdy.print();
+  console.log(`listening on port: ${PORT}`);
+});
