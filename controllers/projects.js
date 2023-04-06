f00c534bf1ca6913da859f5c39abcb15a2ad7f35
dxk0ta <dxkotacamps@gmail.com>
2023-04-06 11:26:37 -0700

    hw

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,39 +2,62 @@ let express = require('express')
 let db = require('../models')
 let router = express.Router()
 
-// POST /projects - create a new project
-router.post('/', (req, res) => {
-  db.project.create({
-    name: req.body.name,
-    githubLink: req.body.githubLink,
-    deployLink: req.body.deployedLink,
-    description: req.body.description
-  })
-  .then((project) => {
-    res.redirect('/')
-  })
-  .catch((error) => {
-    res.status(400).render('main/404')
-  })
-})
-
-// GET /projects/new - display form for creating a new project
+// GET /projects/new - display form for creating a new project with a category field
 router.get('/new', (req, res) => {
   res.render('projects/new')
 })
 
 // GET /projects/:id - display a specific project
-router.get('/:id', (req, res) => {
+router.get('/', (req, res) => {
   db.project.findOne({
-    where: { id: req.params.id }
+    where: { id: req.params.id },
+    include: [db.category]
   })
   .then((project) => {
     if (!project) throw Error()
     res.render('projects/show', { project: project })
   })
   .catch((error) => {
+    console.log(error)
     res.status(400).render('main/404')
   })
 })
 
+// POST /projects - create a new project with a category
+router.post('/', async (req, res) => {
+  try {
+    // no db implied. I have category the variable and I need to use it in line 33
+    const [category] = await db.category.findOrCreate({
+      where: { name: req.body.category }
+    })
+     await category.createProject({
+      name: req.body.name,
+      githubLink: req.body.githubLink,
+      deployLink: req.body.deployedLink,
+      description: req.body.description,
+    })
+    res.redirect('../')
+  } catch (error) {
+    res.status(400).render('main/404')
+    console.log(error)
+  }
+})
+
+// router.post('/', async (req, res) => {
+//   try {
+//     const [category] = await db.category.findOrCreate({
+//       where: { name: req.body.category }
+//     })
+//     const project = await db.project.create({
+//       name: req.body.name,
+//       githubLink: req.body.githubLink,
+//       deployLink: req.body.deployedLink,
+//       description: req.body.description,
+//     })
+//     await project.addCategory(category)
+//     res.redirect('/')
+//   } catch (error) {
+//     res.status(400).render('main/404')
+//   }
+// })
 module.exports = router
