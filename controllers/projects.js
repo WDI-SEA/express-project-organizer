a081864aa5bab12f50b5a8a8ece31d196cc330fb
andrewbantly <andrewbantly@gmail.com>
2023-04-05 20:12:01 -0700

    Andrew Bantly's deliverable

--- controllers/projects.js
+++ controllers/projects.js
@@ -3,19 +3,26 @@ let db = require('../models')
 let router = express.Router()
 
 // POST /projects - create a new project
-router.post('/', (req, res) => {
-  db.project.create({
+router.post('/', async (req, res) => {
+  try {
+  console.log(req.body);
+  const newProject = await db.project.create( {
     name: req.body.name,
     githubLink: req.body.githubLink,
     deployLink: req.body.deployedLink,
-    description: req.body.description
+    description: req.body.description,
   })
-  .then((project) => {
-    res.redirect('/')
+  const [newCategory] = await db.category.findOrCreate({
+    where: {
+      name: req.body.category
+    }
   })
-  .catch((error) => {
+  // returns [newCategory, true/false]
+  await newProject.addCategory(newCategory);
+  res.redirect('/')
+} catch (err) {
     res.status(400).render('main/404')
-  })
+  }
 })
 
 // GET /projects/new - display form for creating a new project
