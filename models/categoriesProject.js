bf842aaacecf399ec0c9daf6baebf31a95b95487
jaronw1 <jaronwong6@gmail.com>
2023-04-15 14:53:29 -0700

    deliv

---
+++ models/categoriesProject.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class categoriesProject extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  categoriesProject.init({
+    categoryId: DataTypes.INTEGER,
+    projectId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'categoriesProject',
+  });
+  return categoriesProject;
+};
--- models/project.js
+++ models/project.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.category.belongsToMany(models.project, {through: 'categoriesProject'})
     }
   }
   project.init({
